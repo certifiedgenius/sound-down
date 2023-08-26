d48145604515a7230944c86825903d977b6917a2
certifiedgenius <mugeeramansoor@live.com>
2023-08-27 00:41:11 +0200

    removing all spotify and auth related code

--- sound-down/src/components/Playlist.tsx
+++ sound-down/src/components/Playlist.tsx
@@ -1,13 +1,10 @@
-// src/components/Playlist.tsx
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { MusicPlayerContext } from '../contexts/MusicPlayerContext';
-import { useAuth, LoginButton } from 'react-oauth2-login';
-import axios from 'axios';
 
 interface Playlist {
   id: string;
   name: string;
-  tracks: TrackInfo[]; // Using a unique name for the track type
+  tracks: TrackInfo[];
 }
 
 interface TrackInfo {
@@ -19,62 +16,8 @@ interface TrackInfo {
 
 const PlaylistComponent: React.FC = () => {
   const musicPlayerContext = useContext(MusicPlayerContext);
-  const auth = useAuth();
-  const [playlists, setPlaylists] = useState<Playlist[]>([]);
-  const [selectedPlaylist, setSelectedPlaylist] = useState<Playlist | null>(null);
-  const [tracks, setTracks] = useState<TrackInfo[]>([]);
-  
 
-  useEffect(() => {
-    if (auth) {
-      const fetchPlaylists = async () => {
-        try {
-          const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
-            headers: {
-              Authorization: `Bearer ${auth}`,
-            },
-          });
-          const fetchedPlaylists: Playlist[] = response.data.items.map((item: any) => ({
-            id: item.id,
-            name: item.name,
-            tracks: [], // Initialize with empty tracks array
-          }));
-          setPlaylists(fetchedPlaylists);
-        } catch (error) {
-          console.error('Error fetching playlists:', error);
-        }
-      };
-
-      fetchPlaylists();
-    }
-  }, [auth]);
-  
-  const handlePlaylistClick = async (playlist: Playlist) => {
-    setSelectedPlaylist(playlist);
-
-    try {
-      const response = await axios.get(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
-        headers: {
-          Authorization: `Bearer ${auth}`,
-        },
-      });
-
-      const fetchedTracks: TrackInfo[] = response.data.items.map((item: any) => ({
-        id: item.track.id,
-        title: item.track.name,
-        artist: item.track.artists.map((artist: any) => artist.name).join(', '),
-        url: item.track.preview_url,
-      }));
-
-      setTracks(fetchedTracks);
-    } catch (error) {
-      console.error('Error fetching tracks:', error);
-    }
-  };
-  
-  
-
-  if (!musicPlayerContext || !auth) {
+  if (!musicPlayerContext) {
     return null;
   }
 
@@ -83,34 +26,8 @@ const PlaylistComponent: React.FC = () => {
   return (
     <div className="playlist">
       <h2>Playlists</h2>
-      {auth ? (
-        <div>
-          <ul>
-            {playlists.map((playlist) => (
-              <li key={playlist.id} onClick={() => handlePlaylistClick(playlist)}>
-                {playlist.name}
-              </li>
-            ))}
-          </ul>
-          {selectedPlaylist && (
-            <div>
-              <h3>Tracks in {selectedPlaylist.name}</h3>
-              <ul>
-                {tracks.map((track) => (
-                  <li key={track.id}>
-                    {track.title} - {track.artist}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      ) : (
-        <div>
-          <p>You need to authenticate to view playlists.</p>
-          <LoginButton />
-        </div>
-      )}
+      {/* Replace the following part with your desired content */}
+      <p>This is where your playlist content should be rendered.</p>
     </div>
   );
 };
